dcfb51750f9060043f20d18b1ca0e9d91e603f8b
Firdous I <mohd.firdous1995@gmail.com>
2018-01-15 13:58:45 +0530

    From Nexusinfo, Lukhman

--- app/src/main/java/com/nexusinfo/mia_muslimindustrialistassociation/ui/adapters/ProductAdapter.java
+++ app/src/main/java/com/nexusinfo/mia_muslimindustrialistassociation/ui/adapters/ProductAdapter.java
@@ -1,6 +1,8 @@
 package com.nexusinfo.mia_muslimindustrialistassociation.ui.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,6 +39,31 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     @Override
     public void onBindViewHolder(ProductViewHolder holder, int position) {
 
+        byte[] photoData = mProducts.get(position).getPhoto();
+        String productName = mProducts.get(position).getProductName();
+        String companyName = mProducts.get(position).getCompanyName();
+        String productSpecification = mProducts.get(position).getSpecification();
+
+        if(photoData != null) {
+            Bitmap bmp = BitmapFactory.decodeByteArray(photoData, 0, photoData.length);
+            holder.ivProductPhoto.setImageBitmap(bmp);
+        }
+
+        if (productName != null && !productName.equals(""))
+            holder.tvProductName.setText(productName);
+        else
+            holder.tvProductName.setText("-");
+
+        if (companyName != null && !companyName.equals(""))
+            holder.tvCompanyName.setText(companyName);
+        else
+            holder.tvCompanyName.setText("-");
+
+        if (productSpecification != null && !productSpecification.equals(""))
+            holder.tvProductSpecification.setText(productSpecification);
+        else
+            holder.tvProductSpecification.setText("-");
+
     }
 
     @Override
@@ -47,7 +74,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     class ProductViewHolder extends RecyclerView.ViewHolder{
 
         ImageView ivProductPhoto;
-        TextView tvProductName, tvCompanyName, tvProductDescription;
+        TextView tvProductName, tvCompanyName, tvProductSpecification;
 
 
         public ProductViewHolder(View itemView) {
@@ -56,7 +83,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             ivProductPhoto = itemView.findViewById(R.id.imageView_productPhoto);
             tvProductName = itemView.findViewById(R.id.textView_productName);
             tvCompanyName = itemView.findViewById(R.id.textView_companyName);
-            tvProductDescription = itemView.findViewById(R.id.textView_productDescription);
+            tvProductSpecification = itemView.findViewById(R.id.textView_productSpecification);
         }
     }
 }
--- app/src/main/java/com/nexusinfo/mia_muslimindustrialistassociation/ui/fragments/ProductFragment.java
+++ app/src/main/java/com/nexusinfo/mia_muslimindustrialistassociation/ui/fragments/ProductFragment.java
@@ -5,12 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nexusinfo.mia_muslimindustrialistassociation.R;
+import com.nexusinfo.mia_muslimindustrialistassociation.models.ProductModel;
 import com.nexusinfo.mia_muslimindustrialistassociation.ui.activities.AddProductActivity;
+import com.nexusinfo.mia_muslimindustrialistassociation.ui.adapters.ProductAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +29,10 @@ public class ProductFragment extends Fragment {
     }
 
     private View view;
-    private FloatingActionButton fab;
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
+    private List<ProductModel> mProducts;
+    private ProductAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,9 +43,24 @@ public class ProductFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_product, container, false);
 
-        fab = view.findViewById(R.id.fab_add_product);
+        mFab = view.findViewById(R.id.fab_add_product);
+        mRecyclerView = view.findViewById(R.id.recyclerView_product);
 
-        fab.setOnClickListener(v -> {
+        mProducts = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            ProductModel model = new ProductModel();
+            model.setProductName("Nedusoft");
+            model.setCategoryName("Onespot Nexusinfo");
+            model.setSpecification("School ERP software for Management of Schools/Colleges");
+
+            mProducts.add(model);
+        }
+
+        mAdapter = new ProductAdapter(getContext(), mProducts);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mFab.setOnClickListener(v -> {
             Intent addProductIntent = new Intent(getContext(), AddProductActivity.class);
             startActivity(addProductIntent);
         });
